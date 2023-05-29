545e7d9c0c2d9f30050d7a9749848d6d68d1ac1d
AsumVictor <iamasum369@gmail.com>
2023-05-29 16:51:43 +0000

    Install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        veryDarkBlue:' #272a31',
+        lightPink:' #ec5242',
+        ash:' #d3d3d3',
+      }
+    },
+  },
+  plugins: [],
+}
