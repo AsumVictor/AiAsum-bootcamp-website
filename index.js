4c49da43e17b25a208eee16d182fe55ddf5dc9a1
AsumVictor <iamasum369@gmail.com>
2023-05-30 20:29:42 +0000

    Add mobile menu to about page

---
+++ index.js
@@ -0,0 +1,4 @@
+const mobileMenu = document.querySelector('')
+const closeMenu = document.querySelector('')
+const openMenu = document.querySelector('')
+const navLinks = document.querySelectorAll('')
