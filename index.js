242c1ef5d09cdc3ecee767c91aa719f7763e1b4c
AsumVictor <iamasum369@gmail.com>
2023-05-30 20:46:24 +0000

    When user click on humber icon, it opens mobile menu link. When user clicks on close icon it hides the menu links

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-const mobileMenu = document.querySelector('')
-const closeMenu = document.querySelector('')
-const openMenu = document.querySelector('')
-const navLinks = document.querySelectorAll('')
+const mobileMenu = document.querySelector(".mobile-nav");
+const closeMenu = document.querySelector(".close");
+const openMenu = document.querySelector(".humberger");
+const navLinks = document.querySelectorAll(".mobile-links li a");
+
+//When user clicks on humber icon
+openMenu.addEventListener("click", () => {
+  mobileMenu.classList.remove("hidden");
+  mobileMenu.classList.add("flex");
+  document.body.style.overflowY = "hidden";
+});
+
+//When user click on the close button
+closeMenu.addEventListener("click", () => {
+  mobileMenu.classList.remove("flex");
+  mobileMenu.classList.add("hidden");
+  document.body.style.overflowY = "scroll";
+});
+
+//When user clicks on any of the menu links
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("flex");
+    mobileMenu.classList.add("hidden");
+    document.body.style.overflowY = "scroll";
+  });
+});
