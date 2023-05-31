9c2037dafc2a206460cc72c1f4b86b965cb5d16e
AsumVictor <iamasum369@gmail.com>
2023-05-31 07:14:57 +0000

    Add folder to store data of tutors

---
+++ Ds_store/tutors_data.js
@@ -0,0 +1,8 @@
+export let tutors = [
+  {
+    name: "Asum Victor",
+    position: "CEO of AiAsum co-operation LTD",
+    bio: `As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration.`,
+    avatar: "https://avatars.githubusercontent.com/u/105683075?v=4",
+  },
+];
